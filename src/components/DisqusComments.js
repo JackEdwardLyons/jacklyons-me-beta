73d4978ca4c39740b186dfb33e848bf54299fc8b
Jack L <jack.edward.lyons@gmail.com>
2021-05-01 19:58:23 +1000

    Chore: don't show disqus in preview

--- src/components/DisqusComments.js
+++ src/components/DisqusComments.js
@@ -2,17 +2,12 @@ import React from "react";
 import { DiscussionEmbed } from "disqus-react";
 
 const DisqusComments = ({ post }) => {
-  console.log({ post: post.url });
   const disqusShortname = "jacklyons-me";
   const disqusConfig = {
     url: "https://jacks-website-8f246.netlify.app" + post.url,
     identifier: post.id,
     title: post.title,
   };
-
-  if (window && window.location.href.includes("app.stackbit.com/studio")) {
-    return null;
-  }
 
   return (
     <div>
