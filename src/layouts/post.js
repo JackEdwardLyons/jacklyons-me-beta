26fba09729ef47e588a62f16bbd3d0e96133ad80
Jack Lyons <jack.edward.lyons@gmail.com>
2021-09-13 20:59:01 +1000

    Feat: Add Markdown component for embedding subscribe box anywhere in post.
    For more info check out the docs: https://www.npmjs.com/package/markdown-to-jsx
    Goto `options.overrides - Rendering Arbitrary React Components`

--- src/layouts/post.js
+++ src/layouts/post.js
@@ -1,12 +1,16 @@
 import React from "react";
 import _ from "lodash";
 import moment from "moment-strftime";
-
+import reactStringReplace from "react-string-replace";
 import { Layout } from "../components/index";
 import { htmlToReact, withPrefix, markdownify } from "../utils";
-import DisqusComments from "../components/DisqusComments";
+// import DisqusComments from "../components/DisqusComments";
 import Subscribe from "../components/Subscribe";
+import Markdown from "markdown-to-jsx";
 
+const md = `
+# Hello world
+`;
 export default class Post extends React.Component {
   render() {
     const postTitle = _.get(this.props, "page.frontmatter.title", null);
@@ -19,23 +23,49 @@ export default class Post extends React.Component {
             <header className="post-header inner-sm">
               <h1 className="post-title line-top">{postTitle}</h1>
               {_.get(this.props, "page.frontmatter.subtitle", null) && (
-                <div className="post-subtitle">{htmlToReact(_.get(this.props, "page.frontmatter.subtitle", null))}</div>
+                <div className="post-subtitle">
+                  {htmlToReact(
+                    _.get(this.props, "page.frontmatter.subtitle", null)
+                  )}
+                </div>
               )}
             </header>
             {_.get(this.props, "page.frontmatter.image", null) && (
               <div className="post-image">
-                <img src={withPrefix(_.get(this.props, "page.frontmatter.image", null))} alt={_.get(this.props, "page.frontmatter.image_alt", null)} />
+                <img
+                  src={withPrefix(
+                    _.get(this.props, "page.frontmatter.image", null)
+                  )}
+                  alt={_.get(this.props, "page.frontmatter.image_alt", null)}
+                />
               </div>
             )}
             <div className="post-content inner-sm">
-              {markdownify(_.get(this.props, "page.markdown", null))}
+              <Markdown
+                options={{
+                  overrides: {
+                    Subscribe: {
+                      component: Subscribe,
+                    },
+                  },
+                }}
+              >
+                {_.get(this.props, "page.markdown", null)}
+              </Markdown>
 
               <Subscribe />
             </div>
 
             <footer className="post-meta inner-sm">
-              <time className="published" dateTime={moment(_.get(this.props, "page.frontmatter.date", null)).strftime("%Y-%m-%d %H:%M")}>
-                {moment(_.get(this.props, "page.frontmatter.date", null)).strftime("%B %d, %Y")}
+              <time
+                className="published"
+                dateTime={moment(
+                  _.get(this.props, "page.frontmatter.date", null)
+                ).strftime("%Y-%m-%d %H:%M")}
+              >
+                {moment(
+                  _.get(this.props, "page.frontmatter.date", null)
+                ).strftime("%B %d, %Y")}
               </time>
 
               {/* <DisqusComments
